747b69aa64419bb1135596306b2c9d2c3a0ad9a8
Giobeck28 <107695001+Giobeck28@users.noreply.github.com>
2024-06-27 08:56:16 +0200

    Update app_streamlit_23_24.py

--- app_streamlit_23_24.py
+++ app_streamlit_23_24.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+#import numpy as np
 from PIL import Image
 import time
 import plotly.figure_factory as ff
